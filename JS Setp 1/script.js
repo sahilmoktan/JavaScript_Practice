1c8a62868291966b5bb5c7c90f8440405d26bdb5
sahilmoktan <moktanshail9@gmail.com>
2023-10-25 23:33:57 +0545

    practice continue

---
+++ JS Setp 1/script.js
@@ -0,0 +1,38 @@
+// minustopic
+
+// -word vs keyword 
+// baba, yaya or any text that has no meaning in JavaScript is word, these text are not reserved by JS
+// for, var, let or any text that has meaning in JavaScript is keyword, they are reserved by JavaScript
+
+
+// -var const let
+
+// variable and constent 
+// score = 3, life = 2
+
+// code ma data store garna use hune lai variable vaninxa
+
+var a = 12
+var b = 19
+// a = 6
+
+const changeNahune = 'assign gareko data change hudaina'
+const c = 4
+// c = 6
+
+let d = 'variables nai ho but differnt from var , check on double minus the difference'
+
+
+// -hoisting
+// not hosting  !! don't be confuse
+
+// variable and function are hoisted wihch mean there declaration is moved on top of code
+
+console.log(e)
+var e; //declaration
+e =12 //initialization or to set 
+
+//hoisting makes var e at top of all, that's why even if we declare var e below console, it execute var e
+
+
+// vid ko 22min
