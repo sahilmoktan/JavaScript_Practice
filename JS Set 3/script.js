58242056c7f3c1fe47cce90ef4205e290b609d31
sahilmoktan <moktanshail9@gmail.com>
2023-11-13 10:25:11 +0545

    bubble game practice

---
+++ JS Set 3/script.js
@@ -0,0 +1,9 @@
+var clutter = ''
+
+for(var i=1; i<150; i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter+= `<div class="bubble">${rn}</div>`
+
+}
+
+document.querySelector('#pbtm').innerHTML = clutter
