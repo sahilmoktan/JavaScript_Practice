d2a3c25ae81d3b1166902c474ca5917ad5868800
sahilmoktan <moktanshail9@gmail.com>
2023-11-13 13:25:46 +0545

    game made and concept written in readme file

--- JS Set 3/readme.md
+++ JS Set 3/readme.md
@@ -1,1 +1,5 @@
 <!-- Bubble Game - JavaScript-->
+
+concept: 
+there will be Hit that shows number to be clicked,
+timer under it we should click number given on body part, score collect values that we have scored, body part where randomly amout of number is being showing
--- JS Set 3/script.js
+++ JS Set 3/script.js
@@ -1,9 +1,53 @@
-var clutter = ''
+var timer = 6;
+var score = 0;
+var hitrn = 0
 
-for(var i=1; i<150; i++){
-    var rn = Math.floor(Math.random()*10)
-    clutter+= `<div class="bubble">${rn}</div>`
-
+function getNewHit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hitrn;
 }
 
-document.querySelector('#pbtm').innerHTML = clutter
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+
+function makeBubble() {
+  var clutter = "";
+
+  for (var i = 1; i < 150; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(timerint);
+      document.querySelector('#pbtm').innerHTML = `<h1>Game Finish </h1>`
+    }
+  }, 1000);
+}
+
+document.querySelector('#pbtm').addEventListener('click', function(ddd){
+    // alert('it is working')
+    var clickedNum = Number(ddd.target.textContent)
+    if (clickedNum === hitrn){
+        increaseScore()
+        makeBubble()
+        getNewHit()
+    }
+})
+
+
+runTimer();
+
+makeBubble();
+getNewHit();
+increaseScore();
